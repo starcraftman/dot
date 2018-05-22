097d79688ebc556b2635677e5fe0995437dabdeb
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2018-05-22 10:21:58 -0400

    Consistent spacing.

--- files/git-prompt-colors.sh
+++ files/git-prompt-colors.sh
@@ -10,12 +10,12 @@ override_git_prompt_colors() {
 
   GIT_PROMPT_BRANCH="${BoldMagenta}"    # the git branch that is active in the current directory
   GIT_PROMPT_STAGED="${Red}●"           # the number of staged files/directories
-  GIT_PROMPT_CONFLICTS="${Red}✖ "       # the number of files in conflict
-  GIT_PROMPT_CHANGED="${Blue}✚ "        # the number of changed files
+  GIT_PROMPT_CONFLICTS="${Red}✖"       # the number of files in conflict
+  GIT_PROMPT_CHANGED="${Blue}✚"        # the number of changed files
 
   GIT_PROMPT_REMOTE=" "                 # the remote branch name (if any) and the symbols for ahead and behind
   GIT_PROMPT_UNTRACKED="${Cyan}…"       # the number of untracked files/dirs
-  GIT_PROMPT_STASHED="${BoldBlue}⚑ "    # the number of stashed files/dir
+  GIT_PROMPT_STASHED="${BoldBlue}⚑"    # the number of stashed files/dir
   GIT_PROMPT_CLEAN="${BoldGreen}✔"      # a colored flag indicating a "clean" repo
 
   GIT_PROMPT_COMMAND_OK="${Green}✔ "    # indicator if the last command returned with an exit code of 0
